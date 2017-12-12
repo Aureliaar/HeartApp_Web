0bba9c243996a39ca935205489fbbe6757062e96
aureon <jacopo.sinigaglia@gmail.com>
2017-12-13 00:39:16 +0100

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#modal-content').apFullscreenModal({
+        openSelector: '#open-modal-1',
+        backgroundColor: '#fafafa',
+        closeSelector: '.close-modal, #send',
+    });
+});
