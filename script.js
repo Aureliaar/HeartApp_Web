b4a0ca90f803c81b10812ce2a977b48f04a08f84
aureon <jacopo.sinigaglia@gmail.com>
2017-12-14 01:48:02 +0100

    .

--- script.js
+++ script.js
@@ -4,4 +4,31 @@ $(document).ready(function() {
         backgroundColor: '#fafafa',
         closeSelector: '.close-modal, #send',
     });
+    $('.js-example-basic-single').select2({
+        theme: "flat"
+    });
+
+    $('.js-example-basic-multiple').select2({
+        theme: "flat"
+    });
+
+    $("#singleSelectExample").select2({
+        placeholder: 'Select an option...',
+        theme: "flat",
+        allowClear: true
+    });
+    $(".js-example-placeholder-single").select2({
+        placeholder: 'Select an option...',
+        theme: "flat",
+        allowClear: true
+    });
+
+    $(".js-example-placeholder-multiple").select2({
+        placeholder: "Select a option",
+        theme: "flat"
+    });
 });
+/*Select2 Initlization*/
+$('.modal-content').change(
+    function() {alert('something'); // ?
+ });
